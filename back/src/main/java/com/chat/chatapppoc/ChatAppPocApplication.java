03ca4852400898c070d600f116dade7b27a18656
Cecilia Giusti <cecilia@tete-chercheuse.fr>
2024-06-13 21:06:15 +0200

    Add maven project for back

---
+++ back/src/main/java/com/chat/chatapppoc/ChatAppPocApplication.java
@@ -0,0 +1,13 @@
+package com.chat.chatapppoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppPocApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/chat/chatapppoc/ChatAppPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.chatapppoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
